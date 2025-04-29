d0067142d7c72ffd914def1aa60d549a5780c37a
Bruno Silva <brs.rsilva@gmail.com>
2025-04-29 10:22:16 -0300

    feat: click

--- src/components/PdfViewer.js
+++ src/components/PdfViewer.js
@@ -1,11 +1,27 @@
-import { useEffect, useRef } from "react";
 import * as pdfjsLib from "pdfjs-dist/build/pdf";
 import * as pdfjsViewer from "pdfjs-dist/web/pdf_viewer";
 import "pdfjs-dist/web/pdf_viewer.css";
+import { useEffect, useRef } from "react";
 
 export default function PdfViewer() {
   const outerContainerRef = useRef(null);
   const innerViewerRef = useRef(null);
+
+  const getClick = () => {
+    const pageElements = innerViewerRef.current.querySelectorAll('.page');
+
+      pageElements.forEach((pageElement, index) => {
+        pageElement.addEventListener('click', (event) => {
+          const rect = pageElement.getBoundingClientRect();
+          const x = event.clientX - rect.left;
+          const y = event.clientY - rect.top;
+          const pageNum = index + 1;
+
+          console.log(`Você clicou na Página ${pageNum} nas coordenadas X: ${x.toFixed(2)}, Y: ${y.toFixed(2)}`);
+          // alert(`Página: ${pageNum}\nX: ${x.toFixed(2)}\nY: ${y.toFixed(2)}`);
+        });
+      });
+  }
 
   useEffect(() => {
     const DEFAULT_URL = "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf";
@@ -36,37 +52,25 @@ export default function PdfViewer() {
       pdfViewer.setDocument(pdfDocument);
     });
 
-    setTimeout(() => {
-      const pageElements = innerViewerRef.current.querySelectorAll('.page');
-
-      pageElements.forEach((pageElement, index) => {
-        pageElement.addEventListener('click', (event) => {
-          const rect = pageElement.getBoundingClientRect();
-          const x = event.clientX - rect.left;
-          const y = event.clientY - rect.top;
-          const pageNum = index + 1; 
-
-          console.log(`Você clicou na Página ${pageNum} nas coordenadas X: ${x.toFixed(2)}, Y: ${y.toFixed(2)}`);
-          alert(`Página: ${pageNum}\nX: ${x.toFixed(2)}\nY: ${y.toFixed(2)}`);
-        });
-      });
-    }, 500);
-
   }, []);
 
   return (
-    <div
-      ref={outerContainerRef}
-      className="customPdfViewerWrapper"
-      style={{
-        position: "absolute", 
-        minHeight: "100vh",
-        overflow: "auto",
-        backgroundColor: "#808080",
-        padding: "20px 20px 0 20px",
-      }}
-    >
-      <div ref={innerViewerRef} className="pdfViewer singlePageView" />
-    </div>
+    <>
+
+      <div
+        ref={outerContainerRef}
+        className="customPdfViewerWrapper"
+        onClick={getClick}
+        style={{
+          position: "absolute",
+          minHeight: "100vh",
+          overflow: "auto",
+          backgroundColor: "#808080",
+          padding: "20px 20px 0 20px",
+        }}
+        >
+        <div ref={innerViewerRef} className="pdfViewer singlePageView" />
+      </div>
+    </>
   );
 }
